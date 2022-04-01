3f5e914806c9142bbf2e50845d55c94541a05bf7
AdarshJaju154 <77018014+AdarshJaju154@users.noreply.github.com>
2022-04-01 18:54:25 +0530

    New Index page

--- js/menu/sketch.js
+++ js/menu/sketch.js
@@ -15,10 +15,13 @@ window.onresize = function() {
 
 function create_boxes(){
   boxes = new Array(6);
-  for(var i = 0; i < 6; i++){
-    var ang = 2 * PI * i / 6;
-    boxes[i] = new Box(w/3 + r*cos(ang), h/2 + r*sin(ang), 45);
-  }
+  
+  boxes[0] = new Box(w/3 -350 , h/2 + 200, 50);
+  boxes[1] = new Box(w/3 -150 , h/2 + 200, 50);
+  boxes[2] = new Box(w/3 +50, h/2 + 200, 50);
+  boxes[3] = new Box(w/3 -350, h/2 +50, 50);
+  boxes[4] = new Box(w/3 -150, h/2 +50 , 50);
+  boxes[5] = new Box(w/3  +50, h/2 +50 , 50);
   add_information();
 }
 
@@ -41,7 +44,7 @@ function draw() {
   }
   if(ok) cursor(HAND);
   else{
-    show_info(info, 15, w*0.75, h*0.5);
+    show_info(info, 20, w*0.8, h/2 +140);
     cursor(ARROW);
   }
 
@@ -49,8 +52,8 @@ function draw() {
 
 function draw_title(){
 
-  fill('#555555');
-  stroke('#00798c')
+  fill('#57c0ab');
+  stroke('#57c0ab')
   beginShape();
   for(i = 0; i < boxes.length; ++i){
     vertex(boxes[i].x, boxes[i].y);
@@ -60,9 +63,12 @@ function draw_title(){
   strokeWeight(1);
   stroke(0);
   fill(0);
+  textSize(50);
+  text("Motion", w/2- textWidth('Motion')/2, 50);
+  text("Planning", w/2 - textWidth('Planning')/2, 90);
   textSize(25);
-  text("Graph", w/3 - textWidth('Graph')/2, h/2);
-  text("Algorithms", w/3 - textWidth('Algorithms')/2, h/2 + 20);
+  
+  text("https://github.com/AdarshJaju154/ArbieGraphAlgo.git", w/2 - textWidth('https://github.com/AdarshJaju154/ArbieGraphAlgo.git')/2, 170);
   textSize(15);
   text("@ienitk", 5, 20);
 }
@@ -79,7 +85,7 @@ function add_information(){
                     'in the left to try. For a better experience check this in a computer!');
 
   boxes[0].txt = ['DFS'];
-  boxes[0].add_txt(w*0.75, h*0.5,
+  boxes[0].add_txt(w*0.8, h/2 +122.5,
                    'The Depth first search (DFS) is a Graph Traversal Algorithm. ' +
                    'It\'s aim is to traverse the graph in ' +
                    'such a way that it tries to go far from the root node.' ,
@@ -87,7 +93,7 @@ function add_information(){
 
 
   boxes[1].txt = ['BFS'];
-  boxes[1].add_txt(w*0.75, h*0.5,
+  boxes[1].add_txt(w*0.8, h/2 +122.5,
                    'The Breadth first search (BFS) is a Graph ' +
                    'Traversal Algorithm. It\'s aim is to traverse ' +
                    'the graph as close as possible to the root node.',
@@ -95,7 +101,7 @@ function add_information(){
 
 
   boxes[2].txt = ['Dijkstra'];
-  boxes[2].add_txt(w*0.75, h*0.5,
+  boxes[2].add_txt(w*0.8, h/2 +122.5,
                    'Dijkstra\'s algorithm is an algorithm for finding' +
                    ' the shortest paths between nodes in a graph. ' +
                    'For a given source node in the graph, the ' +
@@ -105,14 +111,14 @@ function add_information(){
 
 
   boxes[3].txt = ['Kruskal'];
-  boxes[3].add_txt(w*0.75, h*0.5,
+  boxes[3].add_txt(w*0.8, h/2 +122.5,
                    'Kruskal\s algorithm is a minimum-spanning-tree greedy '+
                    'algorithm which finds an edge of the least possible ' +
                    'weight that connects any two trees in the forest.',
                    'kruskal.html');
 
   boxes[4].txt = ['Bellman', 'Ford'];
-  boxes[4].add_txt(w*0.75, h*0.5,
+  boxes[4].add_txt(w*0.8, h/2 +122.5,
                   'Bellman-Ford algorithm allows you to check whether ' +
                   'there exists a cycle of negative weight in the ' +
                   'graph, and if it does, find one of these cycles.',
@@ -121,7 +127,7 @@ function add_information(){
   
 
   boxes[5].txt = ['A*', 'Search'];
-  boxes[5].add_txt(w*0.75, h*0.5,
+  boxes[5].add_txt(w*0.8, h/2 +122.5,
                   'A* is a graph traversal and path search informed algorithm. ' +
                   'In every step A* selects the path that minimizes a function ' +
                   'that works with a heuristic. ',
